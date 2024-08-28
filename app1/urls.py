11a9cc85d05d336460b5c899d34404339f394419
Ubuntu <ubuntu@ip-172-31-5-104.ap-southeast-2.compute.internal>
2024-08-28 11:39:05 +0000

    Committed the modifications

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from app1.views import *
 
 urlpatterns = [
@@ -20,3 +22,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
+
